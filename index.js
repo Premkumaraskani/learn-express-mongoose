0bd38183f1367e2152fe4cc4e9c0f2c3dc89cf76
Premkumaraskani <78657990+Premkumaraskani@users.noreply.github.com>
2021-02-06 23:13:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
+const app = express();
 
 mongoose
   .connect("mongodb://localhost:27017/acmedb", { useNewUrlParser: true })
